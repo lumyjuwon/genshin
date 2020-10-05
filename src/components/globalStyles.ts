b37569432587a9157c62c104ce250fe6f0955507
An Do Eon <andoune0719@gmail.com>
2020-10-05 11:27:53 +0900

    create gloabl styles with css reset

---
+++ src/components/globalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const GlobalStyles = createGlobalStyle`
+    ${reset};
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        background-color: #2122223;
+    }
+`
