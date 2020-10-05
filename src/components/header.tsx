88471537ab33643510c3763797f61a28a4d358e7
An Do Eon <andoune0719@gmail.com>
2020-10-05 22:31:52 +0900

    make header, but float:right didn't work...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Header() {
-  return (
-    <header>
+  const Header = styled.header`
+    padding: 20px 200px;
+    font-size: 30px;
+    border-bottom: 1px solid #515253;
+    background-color: #111213;
+  `
+  const Inner = styled.div`
+    display: flex;
+  `
 
+  const NavBar = styled.ul`
+    display: flex;
+    align-items: center;
+    // float didn't work..
+    float: right;
+  `
+
+  const NavLink = styled.li`
+    margin-left: 20px;
+  `
+
+  return (
+    <Header>
       <BrowserRouter>
-        <div className="link-section">
-          <ul>
-            <li className="link-home">
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/">Character</Link>
-            </li>
-            <li>
-              <Link to="/">Weapon</Link>
-            </li>
-          </ul>
-        </div>
+        <Inner>
+          <div className="header-title">
+            <Link to="/">Home</Link>
+          </div>
+          <NavBar>
+            <NavLink>
+              <Link to="#">Character</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="#">Weapon</Link>
+            </NavLink>
+          </NavBar>
+        </Inner>
       </BrowserRouter>
-      
-    </header>
+    </Header>
   )
+  
+  
 }
