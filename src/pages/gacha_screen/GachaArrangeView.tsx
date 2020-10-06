603d0ef821f6eb53dcbf182e3f8d7a067a0fa3e1
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-10-07 02:34:14 +0900

    가챠 관련 컴포넌트 및 페이지 수정

---
+++ src/pages/gacha_screen/GachaArrangeView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+interface Props {}
+
+export function GachaArrangeView(props: Props) {
+  const Container = Styled.div({
+    justifyContent: "center",
+  });
+
+  return <Container></Container>;
+}
--- src/pages/gacha_screen/GachaScreen.tsx
+++ src/pages/gacha_screen/GachaScreen.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { RoundImage } from "src/components/image/RoundImage";
 import styled from "styled-components";
 
 export function GachaScreen() {
   const Container = styled.div({});
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <RoundImage
+        image={require("../../resources/characters/images/Amber.png")}
+      />
+    </Container>
+  );
 }
