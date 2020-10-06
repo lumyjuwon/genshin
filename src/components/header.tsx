7199834c58a3273c387ba7ca17cd508444ffc5c2
An Do Eon <andoune0719@gmail.com>
2020-10-07 02:24:50 +0900

    modify css properties

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,12 @@ import styled from "styled-components";
 export function Header({children}: any) {
 
   const Header = styled.header({
+    padding: "0 100px",
     borderBottom: "1px solid #515253",
     backgroundColor: "#111213",
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "20px",
   });
 
   return (
