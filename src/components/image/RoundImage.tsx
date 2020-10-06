55727ee81aff4049e6db69f2647542e9ed9571f7
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-10-07 02:33:56 +0900

    RoundImage Component 추가

--- src/components/image/RoundImage.tsx
+++ src/components/image/RoundImage.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import Styled from "styled-components";
 
-export function RoundImage() {
-  return;
+interface Props {
+  image: any;
+  width?: number;
+  height?: number;
+}
+
+export function RoundImage(props: Props) {
+  const defaultProps: Props = {
+    image: undefined,
+    width: 100,
+    height: 100,
+  };
+
+  const combinedProps: Props = Object.assign({}, defaultProps, props);
+
+  const Image = Styled.img({
+    width: combinedProps.width,
+    height: combinedProps.height,
+    borderRadius: 16,
+  });
+
+  return <Image src={combinedProps.image} />;
 }
