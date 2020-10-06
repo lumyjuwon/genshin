f988f6688398edb2abb258e6b911654d321ae170
An Do Eon <andoune0719@gmail.com>
2020-10-07 02:24:32 +0900

    add styled component for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
 
-export function Footer() {
+export function Footer({children}: any) {
+
+  const Footer = styled.footer({
+    backgroundColor: "#444546",
+    borderTop: "1px solid #616263",
+    marginTop: "20px"
+  });
+
   return (
-    <footer>
-      This is Footer
-    </footer>
-  )
+    <Footer>
+      {children}
+    </Footer>
+  );
 }
