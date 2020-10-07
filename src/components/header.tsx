8973dd77c7745ee7cde725394394e1956bc37f9c
An Do Eon <andoune0719@gmail.com>
2020-10-07 14:06:27 +0900

    make HeaderInner: Cannot use InnerDiv component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-export function Header({children}: any) {
+// import { InnerDiv } from './InnerDiv';
+
+export function Header({ children }: any) {
 
   const Header = styled.header({
-    padding: "0 150px",
     borderBottom: "1px solid #515253",
     backgroundColor: "#111213",
-    display: "flex",
-    alignItems: "center",
     marginBottom: "20px",
   });
 
+  const HeaderInner = styled.div({
+    maxWidth: "1200px",
+    margin: "0 auto",
+    display: "flex",
+    alignItems: "center",
+  })
+
+  // const Test = styled(InnerDiv)({
+  //   display: "flex",
+  //   alignItems: "center"
+  // })
+
   return (
     <Header>
-      {children}
+      <HeaderInner>
+        {children}
+      </HeaderInner>
     </Header>
   );
 }
