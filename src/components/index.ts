9d1b9583bf021b17ebaa6c4570de610bf5cc2a35
An Do Eon <andoune0719@gmail.com>
2020-10-07 22:44:53 +0900

    add new components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,12 @@ export { Text } from "./Text";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { InnerDiv } from './InnerDiv';
+export { CenterAlignDiv } from './CenterAlignDiv';
+
+// Button Component
 export { TextButton } from "./button/TextButton";
 export { TextBoxButton } from "./button/TextBoxButton";
+export { RoundButton } from './button/RoundButton'
+
+// Image Component
+export { RoundImage } from './image/RoundImage';
