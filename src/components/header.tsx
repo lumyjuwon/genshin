36bc704e6e632269f7e79a00387c42f404242327
An Do Eon <andoune0719@gmail.com>
2020-10-07 12:39:22 +0900

    change padding property

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export function Header({children}: any) {
 
   const Header = styled.header({
-    padding: "0 100px",
+    padding: "0 150px",
     borderBottom: "1px solid #515253",
     backgroundColor: "#111213",
     display: "flex",
