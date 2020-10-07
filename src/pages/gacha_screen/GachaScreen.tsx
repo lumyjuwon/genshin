ce132c330666f2fb994e162f031fabd892279a2c
An Do Eon <andoune0719@gmail.com>
2020-10-07 22:45:32 +0900

    apply CenterAlignDiv, RoundButton components

--- src/pages/gacha_screen/GachaScreen.tsx
+++ src/pages/gacha_screen/GachaScreen.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 import { InnerDiv } from "src/components";
-import { RoundImage } from "src/components/image/RoundImage";
+import { RoundImage } from "src/components";
+import { RoundButton } from "src/components";
+import { CenterAlignDiv } from 'src/components';
 
 export function GachaScreen() {
   const Container = styled.div({});
@@ -13,6 +15,12 @@ export function GachaScreen() {
         <RoundImage
           image={require("../../resources/characters/images/Amber.png")}
         />
+        <CenterAlignDiv>
+          <>
+            <RoundButton>Reset</RoundButton>
+            <RoundButton>10Times</RoundButton>
+          </>
+        </CenterAlignDiv>
       </InnerDiv>
     </Container>
   );
