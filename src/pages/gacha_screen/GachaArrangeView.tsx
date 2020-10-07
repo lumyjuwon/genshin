51a336b5ac5ba0bf00cabbf875e6ac683c764d22
An Do Eon <andoune0719@gmail.com>
2020-10-08 01:09:15 +0900

    apply FlexCenterWrapper component to GachaArrangeView

--- src/pages/gacha_screen/GachaArrangeView.tsx
+++ src/pages/gacha_screen/GachaArrangeView.tsx
@@ -1,33 +1,36 @@
 import React from "react";
 import Styled from "styled-components";
 
+import { FlexCenterWrapper } from 'src/components';
+
 interface Props {}
 
 export function GachaArrangeView(props: Props) {
   const GridContainer = Styled.div({
     backgroundColor: "#333",
     display: "grid",
-    maxWidth: "980px",
     height: "400px",
-    margin: "30px auto",
-    padding: "20px 50px",
-    gridTemplateColumns: "repeat(5, 100px)",
+    padding: "25px 50px",
+    gridTemplateColumns: "repeat(5, 150px)",
     columnGap: "10px",
-    rowGap: "20px"
+    rowGap: "20px",
+    textAlign: "center"
   });
 
   return (
-    <GridContainer>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-    </GridContainer>
+    <FlexCenterWrapper>
+      <GridContainer>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </GridContainer>
+    </FlexCenterWrapper>
   );
 }
