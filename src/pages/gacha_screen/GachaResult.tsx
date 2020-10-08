29005bb4766c665fef3c3176136641b3078364fe
An Do Eon <andoune0719@gmail.com>
2020-10-08 18:04:58 +0900

    need to fix: cannot extend styled-component

--- src/pages/gacha_screen/GachaResult.tsx
+++ src/pages/gacha_screen/GachaResult.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FlexCenterWrapper, TextCenterWrapper } from 'src/components';
+
 interface Props {
-  times: number
+  times: number,
+  three: number;
+  four: number;
+  five: number;
 }
 
-export function GachaResult({ times }: Props) {
+export function GachaResult({ times, three, four, five }: Props) {
   const ResultView = styled.div({
-
+    display: "flex"
   });
+
+  const FlexCenterWrapperExtended = styled.div({
+    flex: "1"
+  })
 
   return (
     <ResultView>
-      {`Total Times: ${times}`}
+      <TextCenterWrapper>
+        <>
+          <div>
+            {`Total Times: ${times}`}
+          </div>
+          <div>
+            {`5★: ${five}`}
+          </div>
+          <div>
+            {`4★: ${four}`}
+          </div>
+          <div>
+            {`3★: ${three}`}
+          </div>
+        </>
+      </TextCenterWrapper>
+      <FlexCenterWrapperExtended as={FlexCenterWrapper}>
+        <div>
+          list
+        </div>
+      </FlexCenterWrapperExtended>
     </ResultView>
   );
 }
