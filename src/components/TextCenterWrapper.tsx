386d8399fc1112e91779fb9493c090491a503ce0
An Do Eon <andoune0719@gmail.com>
2020-10-08 18:22:21 +0900

    change props type

--- src/components/TextCenterWrapper.tsx
+++ src/components/TextCenterWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  children: JSX.Element;
+  children: JSX.Element | string;
 }
 
 export function TextCenterWrapper({ children }: Props) {
