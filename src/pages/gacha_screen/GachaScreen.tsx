ac8fa87f0617926024b0e2134c87d0ac0f747308
An Do Eon <andoune0719@gmail.com>
2020-10-09 12:09:38 +0900

    add characters lists

--- src/pages/gacha_screen/GachaScreen.tsx
+++ src/pages/gacha_screen/GachaScreen.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-import { ScreenInnerWrapper } from "src/components";
-import { RoundImage } from "src/components";
-import { RoundButton } from "src/components";
-import { TextCenterWrapper } from 'src/components';
-import { GachaArrangeView } from './GachaArrangeView';
 import { GachaResult } from "./GachaResult";
 import { GachaBanner } from "./GachaBanner";
+import { GachaArrangeView } from './GachaArrangeView';
+import wishesInfo from '../../resources/characters/wishesInfo.json'
+import { ScreenInnerWrapper, RoundButton, TextCenterWrapper } from "src/components";
 
 export function GachaScreen() {
 
@@ -16,22 +14,39 @@ export function GachaScreen() {
   const [ fourStarCount, setFoutStarCount ] = useState(0);
   const [ threeStarCount, setThreeStarCount ] = useState(0); 
 
-  const onResetClick = function(): void {
+  const onResetClick: Function = function(): void {
     setGachaTimes(0);
     setFiveStarCount(0);
     setFoutStarCount(0);
     setThreeStarCount(0);
   };
 
-  const oneTimeClick = function(): void {
+  const oneTimeClick: Function = function(): void {
     setGachaTimes(gachaTimes + 1);
   };
 
-  const tenTimesClick = function(): void {
+  const tenTimesClick: Function = function(): void {
     setGachaTimes(gachaTimes + 10);
   };
 
   const Container = styled.div({});
+
+  // After this, gacha logic
+  const probability: Function = function(prob: number): boolean {
+    return Math.random() < (prob / 100)
+  }
+  const fiveProbability: boolean = probability(0.6);
+  const fourProbability: boolean = probability(5.7);
+
+  const gachaResult = [];
+  
+  if (fiveProbability) {
+    wishesInfo["5starCharacter"]
+  } else if (fourProbability) {
+
+  } else {
+
+  }
 
   return (
     <Container>
