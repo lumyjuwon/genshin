be0ecfa35c0c4a3cd608b5767a04ac20f8dcfaef
An Do Eon <andoune0719@gmail.com>
2020-10-10 18:43:14 +0900

    add direction prop for flex-direction

--- src/components/FlexCenterWrapper.tsx
+++ src/components/FlexCenterWrapper.tsx
@@ -3,18 +3,27 @@ import styled from 'styled-components';
 
 interface Props {
   children: JSX.Element;
+  direction?: string;
 }
 
-export function FlexCenterWrapper({ children }: Props) {
-  const FlexCenterWrapper = styled.div({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  })
+export function FlexCenterWrapper(props: Props) {
+  const defaultProps = {
+    children: undefined,
+    flexDirection: "row"
+  }
+
+  const combinedProps: Props = Object.assign({}, defaultProps, props);
+
+  const FlexCenterWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flexDirection: ${combinedProps.direction};
+  `
 
   return(
     <FlexCenterWrapper>
-      {children}
+      {combinedProps.children}
     </FlexCenterWrapper>
   );
 }
