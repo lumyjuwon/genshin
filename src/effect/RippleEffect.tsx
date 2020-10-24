e8badad28162d4a892cae0eecc2c5c57932697eb
An Do Eon <andoune0719@gmail.com>
2020-10-25 04:03:09 +0900

    create effect folder, Ripple Component

---
+++ src/effect/RippleEffect.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useLayoutEffect } from "react";
+import styled from "styled-components";
+
+interface RippleContainerProps {
+  color: string;
+  duration: number;
+}
+
+const RippleContainer = styled.div<RippleContainerProps>`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  span {
+    transform: scale(0);
+    border-radius: 100%;
+    position: absolute;
+    opacity: 0.6;
+    background-color: ${props => props.color};
+    animation-name: ripple;
+    animation-duration: ${props => props.duration}ms;
+  }
+
+  @keyframes ripple {
+    to {
+      opacity: 0;
+      transform: scale(2);
+    }
+  }
+`;
+
+const useDebouncedRippleCleanUp = (rippleCount: number, duration: number, cleanUpFunction: Function) => {
+  useLayoutEffect(() => {
+    let bounce: number | undefined = undefined;
+    if (rippleCount > 0) {
+      clearTimeout(bounce);
+
+      bounce = setTimeout(() => {
+        cleanUpFunction();
+        clearTimeout(bounce);
+      }, duration * 4);
+    }
+
+    return () => clearTimeout(bounce);
+  }, [rippleCount, duration, cleanUpFunction]);
+};
+
+interface Ripple {
+  x: number;
+  y: number;
+  size: number;
+}
+
+export const Ripple = ({ duration = 850, color = "#999" }) => {
+  const [rippleArray, setRippleArray] = useState<Array<Ripple>>([]);
+
+  useDebouncedRippleCleanUp(rippleArray.length, duration, () => {
+    setRippleArray([]);
+  });
+
+  const addRipple = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    const rippleContainer = event.currentTarget.getBoundingClientRect();
+    const size =
+      rippleContainer.width > rippleContainer.height
+        ? rippleContainer.width
+        : rippleContainer.height;
+    const x = event.pageX - rippleContainer.x - size / 2;
+    const y = event.pageY - rippleContainer.y - size / 2;
+    const newRipple: Ripple = {
+      x,
+      y,
+      size
+    };
+
+    setRippleArray([...rippleArray, newRipple]);
+  };
+
+  return (
+    <RippleContainer duration={duration} color={color} onMouseDown={addRipple}>
+      {rippleArray.length > 0 &&
+        rippleArray.map((ripple, index) => {
+          return (
+            <span
+              key={"span" + index}
+              style={{
+                top: ripple.y,
+                left: ripple.x,
+                width: ripple.size,
+                height: ripple.size
+              }}
+            />
+          );
+        })}
+    </RippleContainer>
+  );
+};
