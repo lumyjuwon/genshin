373ef16fdf0a5f491090c1314fbcf101543cc278
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-10-25 04:52:40 +0900

    Add Image null Function

--- src/components/image/SquareImage.tsx
+++ src/components/image/SquareImage.tsx
@@ -1,29 +1,19 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export interface ImageStyle {
   readonly width?: string;
   readonly height?: string;
   readonly boxShadow?: string;
-  readonly objectFit?:
-    | "inherit"
-    | "none"
-    | "-moz-initial"
-    | "initial"
-    | "revert"
-    | "unset"
-    | "fill"
-    | "contain"
-    | "cover"
-    | "scale-down";
+  readonly objectFit?: 'inherit' | 'none' | '-moz-initial' | 'initial' | 'revert' | 'unset' | 'fill' | 'contain' | 'cover' | 'scale-down';
 }
 
 const Image = styled.img<ImageStyle>((props: ImageStyle) => {
   return {
-    width: props.width || "100px",
-    height: props.height || "100px",
-    boxShadow: props.boxShadow || "none",
-    objectFit: props.objectFit || "fill",
+    width: props.width || '100px',
+    height: props.height || '100px',
+    boxShadow: props.boxShadow || 'none',
+    objectFit: props.objectFit || 'fill'
   };
 });
 
@@ -33,5 +23,10 @@ interface Props {
 }
 
 export function SquareImage(props: Props) {
-  return <Image {...props.styles} src={props.src} />;
+  if (props.src !== '') {
+    return <Image {...props.styles} src={props.src} />;
+  } else {
+    ///@ts-ignore
+    return <Image {...props.styles} src={null} />;
+  }
 }
