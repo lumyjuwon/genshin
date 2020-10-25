ef6ce2b266ab24ffc7bdf5f00a8dfd1b04ff45f4
An Do Eon <andoune0719@gmail.com>
2020-10-25 23:23:05 +0900

    add Route path for footer pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { GachaScreen, PartyScreen, MainScreen } from 'src/pages';
-import { Header, Footer, TextBlockButton, TextUnderLineButton, ContentWrapper, TextCenterWrapper } from 'src/components';
+import { GachaScreen, PartyScreen, MainScreen, Policy, Terms, About } from 'src/pages';
+import { Header, Footer, TextBlockButton, TextUnderLineButton, ContentWrapper, TextCenterWrapper, FlexWrapper } from 'src/components';
 import { trans, Lang } from './resources/languages';
-
-const Container = styled.div({});
 
 const MainLogo = styled.a({
   fontSize: '30px',
@@ -24,20 +21,25 @@ function App() {
           <TextBlockButton href="/party">{trans(Lang.Party)}</TextBlockButton>
         </>
       </Header>
-      <Container>
-        <Switch>
-          <Route path="/gacha" component={GachaScreen} />
-          <Route path="/party" component={PartyScreen} />
-          {/* root path must be the very bottom */}
-          <Route path="/" component={MainScreen} />
-        </Switch>
-      </Container>
+      <Switch>
+        <Route path="/gacha" component={GachaScreen} />
+        <Route path="/party" component={PartyScreen} />
+        <Route path="/policy" component={Policy} />
+        <Route path="/terms" component={Terms} />
+        <Route path="/about" component={About} />
+        {/* root path must be the very bottom */}
+        <Route path="/" component={MainScreen} />
+      </Switch>
       <Footer>
         <>
           <div style={{ fontSize: '12px' }}>Copyrightⓒ 2020</div>
-          <TextUnderLineButton href="/policy">Privacy Policy</TextUnderLineButton>
-          <TextUnderLineButton href="/terms">Terms of Service</TextUnderLineButton>
-          <TextUnderLineButton href="mailto:lumyjuwon@gmail.com">Contact Us</TextUnderLineButton>
+          <FlexWrapper>
+            <>
+            <TextUnderLineButton href="/policy">Privacy Policy</TextUnderLineButton>
+            <TextUnderLineButton href="/terms">Terms of Service</TextUnderLineButton>
+            <TextUnderLineButton href="/about">About Us</TextUnderLineButton>
+            </>
+          </FlexWrapper>
         </>
       </Footer>
     </BrowserRouter>
