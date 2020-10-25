153b1edea94ac6152377ba6d82cd4bd2bbc62ceb
An Do Eon <andoune0719@gmail.com>
2020-10-25 23:24:06 +0900

    apply ImageContentCard

--- src/pages/MainScreen.tsx
+++ src/pages/MainScreen.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { ImageContentCard, TextCenterWrapper } from 'src/components';
 import styled from 'styled-components';
 
-const Container = styled.div({
-  width: "100vw",
-  height: "80vh",
+const ImageContainer = styled.div({
+  width: "100%",
+  height: "90vh",
+  backgroundImage: `url(${require("../resources/images/mainscreen/main.jpg")})`,
+})
+
+const Containter = styled.div({
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0,0,0,.5)",
+  padding: "100px 150px"
+})
+
+const Title = styled.div({
+  fontSize: "50px",
+  textAlign: "center",
+  fontWeight: "bold",
+  marginBottom: "40px"
 })
 
 export function MainScreen() {
   return (
-    <Container>
-      MainScreen
-    </Container>
+    <ImageContainer>
+      <Containter>
+        <Title>Genshin Simulator</Title>
+        <ImageContentCard
+          src={`${require("../resources/images/mainscreen/gacha.png")}`}
+          title="Gacha"
+        />
+        <ImageContentCard
+          src={`${require("../resources/images/mainscreen/party.png")}`}
+          title="Party"
+        />
+      </Containter>
+    </ImageContainer>
   )
 }
