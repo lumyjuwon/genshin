2fc2fc70d7ea0803865a486aec919c1ccdc8df42
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-10-25 21:27:39 +0900

    Add Artifact-info and edit weapon type with capital

--- src/resources/data/index.ts
+++ src/resources/data/index.ts
@@ -1,4 +1,4 @@
-import { GachaData } from "../../pages/gacha/Gacha";
+import { GachaData } from '../../pages/gacha/Gacha';
 
 interface Gacha {
   [key: string]: GachaData;
@@ -7,19 +7,27 @@ interface Gacha {
 export const gachaInfo: Gacha = require('../data/gacha-info.json');
 
 interface Character {
-  [key: string]: {
-    "rank": number,
-    "element": string,
-    "weapon": string,
-    "region": string
-  }
+  [name: string]: {
+    rank: number;
+    element: string;
+    weapon: string;
+    region: string;
+  };
 }
 export const characterInfo: Character = require('../data/character-info.json');
 
 interface Weapon {
-  [key: string]: {
-    rank: number,
-    type: string
-  }
+  [name: string]: {
+    rank: number;
+    type: 'bow' | 'catalyst' | 'claymore' | 'polearm' | 'sword';
+  };
 }
 export const weaponInfo: Weapon = require('../data/weapon-info.json');
+
+interface Artifact {
+  [name: string]: {
+    rank: number;
+    type: string;
+  };
+}
+export const artifactInfo: Artifact = require('../data/artifact-info.json');
