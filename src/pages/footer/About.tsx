abccbee15687065190a0cf58fe28d82738a3dcd1
An Do Eon <andoune0719@gmail.com>
2020-10-26 02:42:36 +0900

    make container of footer pages

--- src/pages/footer/About.tsx
+++ src/pages/footer/About.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div({
+  height: "90vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+});
+
+const Wrapper = styled.div({
+  width: "800px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column"
+})
 
 export function About() {
   return(
-    <div>About</div>
+    <Container>
+      <Wrapper>
+        
+      </Wrapper>
+    </Container>
   );
 }
