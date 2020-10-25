545a1b2e911fc697f99585107ca6a517a8a7ffa1
An Do Eon <andoune0719@gmail.com>
2020-10-25 16:37:42 +0900

    add root directory route

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { GachaScreen, PartyScreen } from 'src/pages';
-import { Header, Footer, TextBlockButton, TextUnderLineButton, ContentWrapper } from 'src/components';
+import { GachaScreen, PartyScreen, MainScreen } from 'src/pages';
+import { Header, Footer, TextBlockButton, TextUnderLineButton, ContentWrapper, TextCenterWrapper } from 'src/components';
 import { trans, Lang } from './resources/languages';
 
 const Container = styled.div({});
@@ -17,29 +17,33 @@ const MainLogo = styled.a({
 function App() {
   return (
     <BrowserRouter>
+      <Header>
+        <>
+          <MainLogo href="/">Genshin Simul</MainLogo>
+          <TextBlockButton href="/gacha">{trans(Lang.Gacha)}</TextBlockButton>
+          <TextBlockButton href="/party">{trans(Lang.Party)}</TextBlockButton>
+        </>
+      </Header>
       <Container>
-        <Header>
-          <>
-            <MainLogo href="/">Genshin Simul</MainLogo>
-            <TextBlockButton href="/gacha">{trans(Lang.Gacha)}</TextBlockButton>
-            <TextBlockButton href="/party">{trans(Lang.Party)}</TextBlockButton>
-          </>
-        </Header>
         <ContentWrapper>
+          <>
           <Switch>
             <Route path="/gacha" component={GachaScreen} />
             <Route path="/party" component={PartyScreen} />
+            {/* root path must be the very bottom */}
+            <Route path="/" component={MainScreen} />
           </Switch>
-        </ContentWrapper>
-        <Footer>
-          <>
-            <div style={{ fontSize: '12px' }}>Copyrightⓒ 2020</div>
-            <TextUnderLineButton href="/policy">Privacy Policy</TextUnderLineButton>
-            <TextUnderLineButton href="/terms">Terms of Service</TextUnderLineButton>
-            <TextUnderLineButton href="mailto:lumyjuwon@gmail.com">Contact Us</TextUnderLineButton>
           </>
-        </Footer>
+        </ContentWrapper>
       </Container>
+      <Footer>
+        <>
+          <div style={{ fontSize: '12px' }}>Copyrightⓒ 2020</div>
+          <TextUnderLineButton href="/policy">Privacy Policy</TextUnderLineButton>
+          <TextUnderLineButton href="/terms">Terms of Service</TextUnderLineButton>
+          <TextUnderLineButton href="mailto:lumyjuwon@gmail.com">Contact Us</TextUnderLineButton>
+        </>
+      </Footer>
     </BrowserRouter>
   );
 }
