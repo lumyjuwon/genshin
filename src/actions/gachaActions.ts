488c54bee2ac4bb4e29b12ce680c31e0ebdcb968
An Do Eon <andoune0719@gmail.com>
2020-10-29 22:09:00 +0900

    init gacha Redux action object and reducer

--- src/actions/gachaActions.ts
+++ src/actions/gachaActions.ts
@@ -1,8 +1,30 @@
+export const ADD_WISH_COUNT = 'ADD_COUNT';
+export const RESET_ALL = 'RESET_ALL';
+export const GACHA_EXECUTION_RESULT = 'GACHA_EXECUTION_RESULT';
+export const SET_WISH_CONTENT = 'SET_WISH_CONTENT';
 
-export const resetButtonClick = () => {
-
+export function addWishCount(count: number) {
+  return {
+    type: ADD_WISH_COUNT,
+    count
+  }
 }
 
-export const wishButttonClick = () => {
-  
+export function resetAll() {
+  return {
+    type: RESET_ALL,
+  }
+}
+
+export function gachaExecutionResult() {
+  return {
+    type: GACHA_EXECUTION_RESULT
+  }
+}
+
+export function setWishContent(content: string) {
+  return {
+    type: SET_WISH_CONTENT,
+    content
+  }
 }
