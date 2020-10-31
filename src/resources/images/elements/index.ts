872c33fdbe2f6d649b44a1efcb84d69b37937bec
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-10-31 22:04:47 +0900

    Add Image Map

--- src/resources/images/elements/index.ts
+++
@@ -1,7 +0,0 @@
-export const Anemo = require("./Anemo.png");
-export const Cryo = require("./Cryo.png");
-export const Dendro = require("./Dendro.png");
-export const Electro = require("./Electro.png");
-export const Geo = require("./Geo.png");
-export const Hydro = require("./Hydro.png");
-export const Pyro = require("./Pyro.png");
--- src/resources/images/index.ts
+++ src/resources/images/index.ts
@@ -1,3 +1,41 @@
-//
-export * from "./elements";
-export * from "./characters";
+import { weaponInfo, artifactInfo, characterInfo } from '../data';
+
+interface ImageMap {
+  [name: string]: string;
+}
+
+function appendImages(map: any, info: any, path: string): void {
+  Object.keys(info).map((name: string) => {
+    if (map[name] === undefined) {
+      map[name] = require(`${path}/${name}.png`);
+    } else {
+      throw new Error(`중복되는 값이 있습니다: ${name}`);
+    }
+  });
+}
+
+const CharacterImages: ImageMap = {};
+appendImages(CharacterImages, characterInfo, './characters');
+
+const ElementImages: ImageMap = {
+  Anemo: require('./elements/Anemo.png'),
+  Cryo: require('./elements/Cryo.png'),
+  Dendro: require('./elements/Dendro.png'),
+  Electro: require('./elements/Electro.png'),
+  Geo: require('./elements/Geo.png'),
+  Hydro: require('./elements/Hydro.png'),
+  Pyro: require('./elements/Pyro.png')
+};
+
+const ItemImages: ImageMap = {};
+appendImages(ItemImages, weaponInfo, './items/weapons');
+appendImages(ItemImages, artifactInfo, './items/artifacts');
+
+const MainScreen = {
+  Gacha: require('./mainscreen/gacha.png'),
+  Main: require('./mainscreen/main.jpg'),
+  Paimon: require('./mainscreen/Paimon.jpg'),
+  Party: require('./mainscreen/party.png')
+};
+
+export { CharacterImages, ElementImages, ItemImages, MainScreen };
