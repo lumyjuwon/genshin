9af69be23e958031b46ff2bd0cd2521a91e2055c
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-11-01 02:00:45 +0900

    Add BuffText and refactory UnderlineInputText

---
+++ src/pages/party/artifact/ArtifactBuffText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ArtifactType } from 'src/resources/data';
+import styled from 'styled-components';
+
+type ArtifactName = string;
+
+interface Props {
+  activeArtifacts: Map<ArtifactType, ArtifactName>;
+}
+
+export function ArtifactBuffText(props: Props) {
+  return null;
+}
