1ec92f134c651072ba7eec3e34f8f8381f36b5b6
An Do Eon <andoune0719@gmail.com>
2020-11-03 12:25:15 +0900

    remove css effects, ref props

--- src/components/button/TextBlockButton.tsx
+++ src/components/button/TextBlockButton.tsx
@@ -31,14 +31,6 @@ const Button = styled.div<ButtonStyle>((props: ButtonStyle) => {
     MozUserSelect: 'none',
     userSelect: 'none',
     transition: '.2s ease-out',
-    '&:hover': {
-      // backgroundColor: props['&:hover']?.backgroundColor || '#515253',
-      textShadow: '0 0 8px #f1f2f3, 0 0 15px #f1f2f3, 0 0 20px #f1f2f3',
-      boxShadow: 'inset 0 -2px 0 #f1f2f3'
-    },
-    '&.selected': {
-      boxShadow: 'inset 0 -2px 0 #f1f2f3'
-    },
     '@media screen and (max-width: 768px)': {
       width: props.small?.width || props.width || 'fit-content',
       padding: '15px 10px'
@@ -52,12 +44,11 @@ interface Props {
     buttonStyles?: ButtonStyle;
   };
   onClick?: Function;
-  refProp?: React.RefObject<HTMLDivElement>;
 }
 
 export function TextBlockButton(props: Props) {
   return (
-    <Button {...props.styles?.buttonStyles} onClick={() => props.onClick?.()} ref={props.refProp}>
+    <Button {...props.styles?.buttonStyles} onClick={() => props.onClick?.()}>
       {props.children}
     </Button>
   );
