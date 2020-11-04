dea0c98a335c270cd027e88ca18364867648d5fc
An Do Eon <andoune0719@gmail.com>
2020-11-04 10:49:12 +0900

    refactoring responsive design for small device

--- src/pages/gacha/GachaResult.tsx
+++ src/pages/gacha/GachaResult.tsx
@@ -18,35 +18,59 @@ const ResultBar = styled.div({
   width: 'fit-content',
   margin: '0 auto',
   borderRadius: '16px',
-  border: '2px dashed #f1f2f3'
+  border: '2px dashed #f1f2f3',
+  '@media screen and (max-width: 768px)': {
+    maxWidth: '100%',
+    overflow: 'hidden'
+  }
 });
 
 const Result = styled.div({
-  margin: '10px 10px',
-  padding: '5px 10px'
+  margin: '10px',
+  padding: '5px 10px',
+  '@media screen and (max-width: 768px)': {
+    margin: '5px',
+    padding: '3px',
+    fontSize: '14px'
+  }
 });
 
 export function GachaResult(props: Props) {
   return (
     <ResultBar>
-      <FlexWrapper
-        styles={{
-          small: { flexDirection: 'column' }
-        }}
-      >
+      <FlexWrapper>
         <>
-          <Result>{`${trans(Lang.Total_Count)}: ${props.times}`}</Result>
-          <Result>{`${trans(Lang.Next_Pity)}: ${props.pity}`}</Result>
-          <Result>{`5★: ${props.five}`}</Result>
-          <Result>{`4★: ${props.four}`}</Result>
-          <Result>
-            <FlexWrapper>
-              <>
-                <SquareImage styles={{ width: '20px', height: '20px' }} src={require('../../resources/images/items/gem/Primogem.webp')} />
-                <span>{`: ${props.gem}`}</span>
-              </>
-            </FlexWrapper>
-          </Result>
+          <FlexWrapper
+            styles={{
+              small: { flexDirection: 'column' }
+            }}
+          >
+            <>
+              <Result>{`${trans(Lang.Total_Count)}: ${props.times}`}</Result>
+              <Result>{`5★: ${props.five}`}</Result>
+              <Result>{`4★: ${props.four}`}</Result>
+            </>
+          </FlexWrapper>
+          <FlexWrapper
+            styles={{
+              small: { flexDirection: 'column' }
+            }}
+          >
+            <>
+              <Result>{`${trans(Lang.Next_Pity)}: ${props.pity}`}</Result>
+              <Result>
+                <FlexWrapper>
+                  <>
+                    <SquareImage
+                      styles={{ width: '20px', height: '20px', small: { width: '18px', height: '18px' } }}
+                      src={require('../../resources/images/items/gem/Primogem.webp')}
+                    />
+                    <span>{`: ${props.gem}`}</span>
+                  </>
+                </FlexWrapper>
+              </Result>
+            </>
+          </FlexWrapper>
         </>
       </FlexWrapper>
     </ResultBar>
