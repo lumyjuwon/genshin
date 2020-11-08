9709c9c543b8c57c3c7d425c6ffb11f9af45897f
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-11-08 22:24:40 +0900

    Remove Selector

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,4 +1,3 @@
 export { store, persistor } from './store';
 
 export { partyDispatch } from './party/dispatch';
-export { partySelector } from './party/selector';
--- src/redux/party/selector.ts
+++
@@ -1,5 +0,0 @@
-import { store } from '../store';
-
-export const partySelector = () => {
-  return store.getState().party;
-};
