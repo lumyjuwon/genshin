0b89fd8df69e88be16b370ebb0ed3485e2a3d9a3
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-11-10 21:13:08 +0900

    Seperate ModalWrapper

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,49 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { CloseBlackButton } from 'src/resources/svg';
 
 import { CircleButton } from '../button/CircleButton';
-
-const Wrapper = styled.div({
-  width: '100vw',
-  height: '100vh',
-  position: 'fixed',
-  overflowY: 'scroll',
-  overflowX: 'hidden',
-  zIndex: 1000,
-  top: 0,
-  right: 0,
-  display: 'flex',
-  tabIndex: -1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  '@media screen and (max-width: 500px)': {
-    alignItems: 'flex-start'
-  }
-});
-
-const WrapperInner = styled.div({
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  boxShadow: '0px 0px 4px 4px rgba(0,0,0,0.25)',
-  padding: '16px',
-  width: 'fit-content',
-  height: 'fit-content',
-  margin: '0 auto',
-  backgroundColor: '#2B2B2B',
-  borderRadius: '12px',
-  '@media screen and (max-width: 1380px)': {
-    width: '80%',
-    justifyContent: 'center'
-  },
-  '@media screen and (max-width: 768px)': {
-    width: '100%',
-    padding: '0'
-  }
-});
+import { ModalWrapper } from '../wrapper/ModalWrapper';
 
 const Header = styled.div({
   display: 'flex',
@@ -54,37 +15,22 @@ const Header = styled.div({
 interface Props {
   visible: boolean;
   cancel: Function;
-  children?: JSX.Element;
-  onCancel?: Function;
+  children: JSX.Element;
 }
 
 export function Modal(props: Props) {
-  useEffect(() => {
-    if (props.visible) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-  }, [props.visible]);
-
-  if (props.visible) {
-    return (
-      <Wrapper>
-        <WrapperInner>
-          <Header>
-            <CircleButton
-              onClick={() => {
-                props.cancel();
-              }}
-            >
-              <CloseBlackButton />
-            </CircleButton>
-          </Header>
-          {props.children}
-        </WrapperInner>
-      </Wrapper>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <ModalWrapper visible={props.visible}>
+      <Header>
+        <CircleButton
+          onClick={() => {
+            props.cancel();
+          }}
+        >
+          <CloseBlackButton />
+        </CircleButton>
+      </Header>
+      {props.children}
+    </ModalWrapper>
+  );
 }
---
+++ src/components/wrapper/ModalWrapper.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div({
+  width: '100vw',
+  height: '100vh',
+  position: 'fixed',
+  overflowX: 'hidden',
+  zIndex: 1000,
+  top: 0,
+  right: 0,
+  display: 'flex',
+  tabIndex: -1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  '@media screen and (max-width: 500px)': {
+    alignItems: 'flex-start',
+    overflowY: 'scroll'
+  }
+});
+
+const WrapperInner = styled.div({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  boxShadow: '0px 0px 4px 4px rgba(0,0,0,0.25)',
+  padding: '16px',
+  width: 'fit-content',
+  height: 'fit-content',
+  margin: '0 auto',
+  backgroundColor: '#2B2B2B',
+  borderRadius: '12px',
+  '@media screen and (max-width: 1380px)': {
+    width: '80%',
+    justifyContent: 'center'
+  },
+  '@media screen and (max-width: 768px)': {
+    width: '100%',
+    padding: '0'
+  }
+});
+
+interface Props {
+  visible: boolean;
+  children: JSX.Element | JSX.Element[] | undefined;
+}
+
+export function ModalWrapper(props: Props) {
+  if (props.visible) {
+    return (
+      <Wrapper>
+        <WrapperInner>{props.children}</WrapperInner>
+      </Wrapper>
+    );
+  } else {
+    return null;
+  }
+}
+
+ModalWrapper.defaultProps = {
+  visible: false
+};
