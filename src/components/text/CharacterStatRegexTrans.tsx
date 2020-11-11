5ac0f9834349807ff4bba6045a9599b3c20d9ae0
An Do Eon <andoune0719@gmail.com>
2020-11-11 21:35:44 +0900

    make CharacterStatRegexTrans component

--- src/components/text/CharacterStatRegexTrans.tsx
+++ src/components/text/CharacterStatRegexTrans.tsx
@@ -12,15 +12,16 @@ interface Props {
   children: string;
 }
 
-export function characterStatRegexText(props: Props) {
+export function CharacterStatRegexText(props: Props) {
   let statPercent: string = '';
   // @ts-ignore
   if (props.children.match(/\d.+%/g)?.length > 0) statPercent = props.children.match(/\d.+%/g)[0];
 
+  console.log(props.children.replace(/\s/g, '_').replace(/\d.+%/g, ''));
   return (
     <P>
       <RegexColorText regex={/\d.+%/g} color={'#cc0000'} isBold>
-        {trans(Lang[props.children.replace(/\s/g, '_').replace(/\d.+%/g, '') as KeyLang]).concat(statPercent)}
+        {trans(Lang[props.children.replace(/\s/g, '_').replace(/_\d.+%/g, '') as KeyLang]).concat(` ${statPercent}`)}
       </RegexColorText>
     </P>
   );
