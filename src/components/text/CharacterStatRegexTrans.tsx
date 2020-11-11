3c3bb423b67633f31734c57c6935a9f4f6814662
An Do Eon <andoune0719@gmail.com>
2020-11-11 13:56:45 +0900

    Genshin Update 1.1 version gacha data update

---
+++ src/components/text/CharacterStatRegexTrans.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { RegexColorText } from './RegexColorText';
+import { trans, Lang, KeyLang } from 'src/resources/languages';
+
+const P = styled.p({
+  lineHeight: '140%'
+});
+
+interface Props {
+  children: string;
+}
+
+export function characterStatRegexText(props: Props) {
+  let statPercent: string = '';
+  // @ts-ignore
+  if (props.children.match(/\d.+%/g)?.length > 0) statPercent = props.children.match(/\d.+%/g)[0];
+
+  return (
+    <P>
+      <RegexColorText regex={/\d.+%/g} color={'#cc0000'} isBold>
+        {trans(Lang[props.children.replace(/\s/g, '_').replace(/\d.+%/g, '') as KeyLang]).concat(statPercent)}
+      </RegexColorText>
+    </P>
+  );
+}
