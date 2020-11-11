42ff44bf4608fb56609bf0dc9827ad12c5c259ab
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-11-12 01:00:45 +0900

    Add isDev function

--- src/components/hooks/useHandleClickOutside.tsx
+++ src/components/hooks/useHandleClickOutside.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 
 export function useHandleClickOutside(ref: React.RefObject<HTMLElement>, className: string, node: any = ref) {
   useEffect(() => {
-    console.log('custom');
     function handleClickOutside(event: MouseEvent) {
       if (node.classList?.contains(className) && !ref.current?.contains(event.target as Node)) {
         node.classList.remove(className);
--- src/utils/combineProps.ts
+++
@@ -1,6 +0,0 @@
-export function combineProps(defaultProps: any, elementProps: any): object {
-  return {
-    ...defaultProps,
-    ...elementProps,
-  }
-}
