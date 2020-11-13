70d0a5fd743207d0053d6f967fa81b1c37af4f87
An Do Eon <andoune0719@gmail.com>
2020-11-14 01:20:01 +0900

    init Daily Screen

---
+++ src/pages/daily/CharacterDaily.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function CharacterDaily() {
+  return <div></div>;
+}
---
+++ src/pages/daily/DailyScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ContentWrapper } from 'src/components';
+
+interface Props {}
+
+export function Daily(props: Props) {
+  return (
+    <ContentWrapper>
+      <div>Daily</div>
+    </ContentWrapper>
+  );
+}
