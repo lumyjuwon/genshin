c93230424f126f96a245e80975c56b7e82a39f0a
An Do Eon <andoune0719@gmail.com>
2020-11-15 13:02:36 +0900

    add width props

--- src/components/wrapper/GridWrapper.tsx
+++ src/components/wrapper/GridWrapper.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div({
-  display: 'flex',
-  flexWrap: 'wrap',
-  width: '40vw',
-  justifyContent: 'center',
-  alignItems: 'center',
-  '@media screen and (max-width: 1380px)': {
-    width: '90%'
-  },
-  '@media screen and (max-width: 768px)': {
-    width: '100%'
-  }
+interface Styles {
+  width?: string;
+  medium?: {
+    width?: string;
+  };
+  small?: {
+    width?: string;
+  };
+}
+
+const Wrapper = styled.div<Styles>((props) => {
+  return {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: props.width || '40vw',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '@media screen and (max-width: 1380px)': {
+      width: props.medium?.width || '90%'
+    },
+    '@media screen and (max-width: 768px)': {
+      width: props.small?.width || '100%'
+    }
+  };
 });
 
 interface Props {
   children: any;
+  styles?: Styles;
 }
 
 export function GridWrapper(props: Props) {
-  return <Wrapper>{props.children}</Wrapper>;
+  return <Wrapper {...props.styles}>{props.children}</Wrapper>;
 }
