77dbe8be5fe7905add924fc9dbb41aca313d23d5
An Do Eon <andoune0719@gmail.com>
2020-11-16 17:10:25 +0900

    make character ascension farming table

--- src/pages/character/CharacterAscesion.tsx
+++ src/pages/character/CharacterAscesion.tsx
@@ -1,16 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { characterAscensionItemInfo, characterInfo } from 'src/resources/data';
+import { CharacterAscentionItemImages } from 'src/resources/images';
+import { RoundImage, RoundImageBox, GridWrapper } from 'src/components';
+import { CharacterImages } from 'src/resources/images';
+
+const Container = styled.div({
+  display: 'flex',
+  width: '100%',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '50px 0 0'
+});
+
 const GridTable = styled.div({
   display: 'grid',
-  gridTemplateColumns: '100px 200px 600px',
-  gridTemplateRows: 'repeat(auto-fit, 100px)',
+  gridTemplateColumns: '70px 170px 100px 450px',
+  gridTemplateRows: 'repeat(auto-fit)',
   width: 'max-content',
-  margin: '30px auto'
+  rowGap: '10px',
+  margin: '30px auto',
+  justifyItems: 'center',
+  alignItems: 'center'
+});
+
+const Title = styled.div({
+  fontSize: '20px',
+  fontWeight: 'bold'
+});
+
+const RelativeBox = styled.div({
+  position: 'relative'
+});
+
+const ItemName = styled.div({
+  fontSize: '14px'
 });
 
 interface Props {}
 
 export function CharacterAscesion(props: Props) {
-  return <GridTable>CharacterAscesion</GridTable>;
+  const ascensionItems = Object.keys(characterAscensionItemInfo);
+  const characters = Object.keys(characterInfo);
+
+  return (
+    <Container>
+      <Title>Character Ascension Items</Title>
+      <GridTable>
+        <div>Item</div>
+        <div>Name</div>
+        <div>Region</div>
+        <div>Characters</div>
+        {ascensionItems.map((item) => {
+          return (
+            <>
+              <RelativeBox>
+                <RoundImage src={CharacterAscentionItemImages[item]} styles={{ width: '60px', height: '60px' }} />
+              </RelativeBox>
+              <ItemName>{item}</ItemName>
+              <div>{characterAscensionItemInfo[item].region}</div>
+              <GridWrapper styles={{ width: '100%' }}>
+                {characters.map((character) => {
+                  if (characterInfo[character].ascension.items.includes(item)) {
+                    return (
+                      <RoundImageBox
+                        src={CharacterImages[character]}
+                        styles={{ boxStyle: { margin: '0', border: 'none' }, imageStyle: { width: '60px', height: '60px' } }}
+                      />
+                    );
+                  } else return null;
+                })}
+              </GridWrapper>
+            </>
+          );
+        })}
+      </GridTable>
+    </Container>
+  );
 }
