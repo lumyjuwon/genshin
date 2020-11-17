119ca661db5e0ffbc979e9053ef9c80197f91bf2
An Do Eon <andoune0719@gmail.com>
2020-11-17 15:17:06 +0900

    add titile

--- src/pages/character/modal/AscensionDetail.tsx
+++ src/pages/character/modal/AscensionDetail.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { FlexWrapper, RoundImage } from 'src/components';
 import { characterInfo } from 'src/resources/data';
 import { CharacterAscentionItemImages } from 'src/resources/images';
 
-interface Props {
-  character: string;
-}
+const Title = styled.div({
+  width: '100%',
+  textAlign: 'center',
+  fontSize: '20px'
+});
 
 enum AscensionStep {
   first = 'first',
@@ -16,8 +19,11 @@ enum AscensionStep {
   fifth = 'fifth',
   sixth = 'sixth'
 }
-
 type Step = keyof typeof AscensionStep;
+
+interface Props {
+  character: string;
+}
 
 export function AscensionDetail(props: Props) {
   let ascensionItems: Map<string, number> = new Map();
@@ -33,26 +39,29 @@ export function AscensionDetail(props: Props) {
   }
 
   return (
-    <FlexWrapper styles={{ flexDirection: 'column' }}>
-      {Object.values(AscensionStep).map((step: Step) => {
-        return (
-          <FlexWrapper styles={{ margin: '10px 0 0' }}>
-            <>
-              <FlexWrapper styles={{ width: '70px' }}>{AscensionStep[step]}</FlexWrapper>
-              <FlexWrapper styles={{ width: '400px' }}>
-                {getAscensionItems(AscensionStep[step]).map((item: string) => {
-                  return (
-                    <FlexWrapper styles={{ margin: '0 3px' }}>
-                      <RoundImage src={CharacterAscentionItemImages[item]} styles={{ width: '60px', height: '60px' }} />
-                      <div style={{ margin: '0 0 0 3px' }}>x{ascensionItems.get(item)}</div>
-                    </FlexWrapper>
-                  );
-                })}
-              </FlexWrapper>
-            </>
-          </FlexWrapper>
-        );
-      })}
+    <FlexWrapper styles={{ flexDirection: 'column', margin: '15px 0 0' }}>
+      <>
+        <Title>Ascension Steps</Title>
+        {Object.values(AscensionStep).map((step: Step) => {
+          return (
+            <FlexWrapper styles={{ margin: '10px 0 0' }}>
+              <>
+                <FlexWrapper styles={{ width: '70px' }}>{AscensionStep[step]}</FlexWrapper>
+                <FlexWrapper styles={{ width: '400px' }}>
+                  {getAscensionItems(AscensionStep[step]).map((item: string) => {
+                    return (
+                      <FlexWrapper styles={{ margin: '0 3px' }}>
+                        <RoundImage src={CharacterAscentionItemImages[item]} styles={{ width: '60px', height: '60px' }} />
+                        <div style={{ margin: '0 0 0 3px' }}>x{ascensionItems.get(item)}</div>
+                      </FlexWrapper>
+                    );
+                  })}
+                </FlexWrapper>
+              </>
+            </FlexWrapper>
+          );
+        })}
+      </>
     </FlexWrapper>
   );
 }
--- src/pages/character/modal/TalentDetail.tsx
+++ src/pages/character/modal/TalentDetail.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { FlexWrapper, RoundImage } from 'src/components';
 import { characterTalentItemInfo, alchemyItemInfo, characterInfo } from 'src/resources/data';
 import { CharacterAscentionItemImages, WeeklyBossItemImages } from 'src/resources/images';
+
+const Title = styled.div({
+  width: '100%',
+  fontSize: '20px',
+  textAlign: 'center',
+  margin: '15px 0 0'
+});
 
 interface Props {
   character: string;
@@ -12,37 +20,40 @@ export function TalentDetail(props: Props) {
   const characterTalentInfo = characterInfo[props.character].talent;
 
   return (
-    <FlexWrapper styles={{ margin: '10px 0 0' }}>
-      <FlexWrapper styles={{ flexDirection: 'column' }}>
-        <FlexWrapper>
-          {Object.keys(characterTalentItemInfo[characterTalentInfo.book].items).map((item) => {
-            return (
-              <RoundImage
-                src={require(`../../../resources/images/items/character-talent/${item}.png`)}
-                styles={{ width: '60px', height: '60px' }}
-              />
-            );
-          })}
+    <>
+      <Title>Talent Level Up Items</Title>
+      <FlexWrapper styles={{ margin: '10px 0 0' }}>
+        <FlexWrapper styles={{ flexDirection: 'column' }}>
+          <FlexWrapper>
+            {Object.keys(characterTalentItemInfo[characterTalentInfo.book].items).map((item) => {
+              return (
+                <RoundImage
+                  src={require(`../../../resources/images/items/character-talent/${item}.png`)}
+                  styles={{ width: '60px', height: '60px' }}
+                />
+              );
+            })}
+          </FlexWrapper>
+          <FlexWrapper>{characterTalentInfo.book}</FlexWrapper>
         </FlexWrapper>
-        <FlexWrapper>{characterTalentInfo.book}</FlexWrapper>
-      </FlexWrapper>
-      <FlexWrapper styles={{ margin: '0 0 0 10px', flexDirection: 'column' }}>
-        <FlexWrapper>
-          <RoundImage src={CharacterAscentionItemImages[characterTalentInfo.drop]} styles={{ width: '60px', height: '60px' }} />
-          <>
-            {Object.keys(alchemyItemInfo)
-              .filter((item) => alchemyItemInfo[item].source === characterTalentInfo.drop)
-              .map((item) => {
-                return <RoundImage src={CharacterAscentionItemImages[item]} styles={{ width: '60px', height: '60px' }} />;
-              })}
-          </>
+        <FlexWrapper styles={{ margin: '0 0 0 10px', flexDirection: 'column' }}>
+          <FlexWrapper>
+            <RoundImage src={CharacterAscentionItemImages[characterTalentInfo.drop]} styles={{ width: '60px', height: '60px' }} />
+            <>
+              {Object.keys(alchemyItemInfo)
+                .filter((item) => alchemyItemInfo[item].source === characterTalentInfo.drop)
+                .map((item) => {
+                  return <RoundImage src={CharacterAscentionItemImages[item]} styles={{ width: '60px', height: '60px' }} />;
+                })}
+            </>
+          </FlexWrapper>
+          <FlexWrapper>{characterTalentInfo.drop}</FlexWrapper>
         </FlexWrapper>
-        <FlexWrapper>{characterTalentInfo.drop}</FlexWrapper>
+        <FlexWrapper styles={{ margin: '0 0 0 10px', flexDirection: 'column' }}>
+          <RoundImage src={WeeklyBossItemImages[characterTalentInfo.boss]} styles={{ width: '60px', height: '60px' }} />
+          <FlexWrapper>{characterTalentInfo.boss}</FlexWrapper>
+        </FlexWrapper>
       </FlexWrapper>
-      <FlexWrapper styles={{ margin: '0 0 0 10px', flexDirection: 'column' }}>
-        <RoundImage src={WeeklyBossItemImages[characterTalentInfo.boss]} styles={{ width: '60px', height: '60px' }} />
-        <FlexWrapper>{characterTalentInfo.boss}</FlexWrapper>
-      </FlexWrapper>
-    </FlexWrapper>
+    </>
   );
 }
