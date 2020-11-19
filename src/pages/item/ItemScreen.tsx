e464083e07247d259c67875cbe0c307e200364f5
An Do Eon <andoune0719@gmail.com>
2020-11-19 21:49:12 +0900

    align items center

--- src/pages/item/ItemScreen.tsx
+++ src/pages/item/ItemScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { ContentWrapper } from 'src/components';
 import { DailyItem } from './DailyItem';
@@ -6,15 +7,26 @@ import { FieldMonsterDropItem } from './FieldMonsterDropItem';
 import { WeeklyRewardItem } from './WeeklyRewardItem';
 import { LocalContainer } from './local_special/LocalContainer';
 
+const Inner = styled.div({
+  width: '100%',
+  minHeight: '80vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
+
 interface Props {}
 
 export function ItemScreen(props: Props) {
   return (
     <ContentWrapper>
-      <DailyItem />
-      <WeeklyRewardItem />
-      <FieldMonsterDropItem />
-      <LocalContainer />
+      <Inner>
+        <DailyItem />
+        <WeeklyRewardItem />
+        <FieldMonsterDropItem />
+        <LocalContainer />
+      </Inner>
     </ContentWrapper>
   );
 }
