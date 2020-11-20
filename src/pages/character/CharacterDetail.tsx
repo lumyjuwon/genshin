873ede33ee90fedd5fe0eed0007ccf9ed6eb1e04
An Do Eon <andoune0719@gmail.com>
2020-11-21 02:39:42 +0900

    component routing with react-router-dom

--- src/pages/character/CharacterDetail.tsx
+++ src/pages/character/CharacterDetail.tsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+interface Props {
+  character: string;
+}
+
+export function CharacterDetail(props: Props) {
+  return <div>{props.character}</div>;
+}
--- src/pages/character/CharacterList.tsx
+++ src/pages/character/CharacterList.tsx
@@ -1,109 +1,63 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import { characterInfo } from 'src/resources/data';
 import { CharacterImages, ElementImages } from 'src/resources/images';
-import { FlexWrapper, SquareImage, BoxModelWrapper, ItemBadgeBox, RoundImage } from 'src/components';
+import { FlexWrapper, SquareImage, BoxModelWrapper, ItemBadgeBox, RoundImage, GridWrapper } from 'src/components';
 import { display } from 'html2canvas/dist/types/css/property-descriptors/display';
 
-const Relative = styled.div({
-  position: 'relative',
+const Container = styled.div({
   display: 'flex',
-  width: '1020px',
-  height: '120px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '1000px',
+  minHeight: '80vh',
   margin: '0 auto',
-  justifyContent: 'space-between',
-  alignItems: 'center'
-});
-
-const HorizontalScroll = styled.div({
-  position: 'absolute',
-  display: 'flex',
-  flexDirection: 'column',
-  top: 0,
-  left: 0,
-  width: '120px',
-  height: '900px',
-  overflowY: 'auto',
-  overflowX: 'hidden',
-  transform: 'rotate(-90deg) translateY(-60px)',
-  transformOrigin: 'right top',
-  scrollbarWidth: 'none',
-  scrollBehavior: 'smooth',
-  '&>*': {
-    transform: 'rotate(90deg)'
-  },
-  '&::-webkit-scrollbar': {
-    width: 0,
-    height: 0
-  }
-});
-
-const ScrollButton = styled.div({
-  width: '50px',
-  padding: '10px',
-  fontSize: '30px',
-  textAlign: 'center',
-  cursor: 'pointer',
-  borderRadius: '8px',
-  transition: '.2s',
-  '&:hover': {
-    backgroundColor: '#f1f2f3',
-    color: '#222324'
-  }
+  flexFlow: 'wrap'
 });
 
 export function CharacterList() {
   const characters = Object.keys(characterInfo);
-  const scrollableRef = useRef<HTMLDivElement>(null);
-
-  function onScrollButtonClick(scrollGap: number) {
-    scrollableRef.current?.scrollBy(0, scrollGap);
-  }
 
   return (
-    <Relative>
-      <ScrollButton onClick={() => onScrollButtonClick(-300)}>
-        <i className="fas fa-chevron-left"></i>
-      </ScrollButton>
-      <HorizontalScroll ref={scrollableRef}>
-        {characters.map((character) => {
+    <Container>
+      <GridWrapper styles={{ width: '100%' }}>
+        {characters.map((name) => {
           return (
-            <ItemBadgeBox
-              badge={
-                <RoundImage
-                  src={ElementImages[characterInfo[character].element]}
-                  styles={{
-                    width: '30px',
-                    height: '30px',
-                    small: {
-                      width: '25px',
-                      height: '25px'
-                    }
-                  }}
-                />
-              }
-              child={<RoundImage src={CharacterImages[character]} />}
-              styles={{
-                boxStyles: { margin: '10px' },
-                badgePosition: { right: '-3px' },
-                tooltipStyles: { bottom: '0' }
-              }}
-              hoverInnerColor={'#f1f2f3'}
-              tooltip={character}
-              onClick={() => alert(character)}
-              isActive={true}
-              isHoverdToolTip={false}
-              isToolTipVisible={true}
-              isRankVisible={false}
-              isBadgeVisible={true}
-            />
+            <Link to={`/character/${name}`}>
+              <ItemBadgeBox
+                badge={
+                  <RoundImage
+                    src={ElementImages[characterInfo[name].element]}
+                    styles={{
+                      width: '30px',
+                      height: '30px',
+                      small: {
+                        width: '25px',
+                        height: '25px'
+                      }
+                    }}
+                  />
+                }
+                child={<RoundImage src={CharacterImages[name]} />}
+                styles={{
+                  boxStyles: { margin: '10px' },
+                  badgePosition: { right: '-3px' },
+                  tooltipStyles: { bottom: '0' }
+                }}
+                hoverInnerColor={'#f1f2f3'}
+                tooltip={name}
+                isActive={true}
+                isHoverdToolTip={true}
+                isToolTipVisible={true}
+                isRankVisible={false}
+                isBadgeVisible={true}
+              />
+            </Link>
           );
         })}
-      </HorizontalScroll>
-      <ScrollButton onClick={() => onScrollButtonClick(300)}>
-        <i className="fas fa-chevron-right"></i>
-      </ScrollButton>
-    </Relative>
+      </GridWrapper>
+    </Container>
   );
 }
