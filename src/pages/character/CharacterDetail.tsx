64bb1241fd17ea903a80c4db747114122fde6fef
An Do Eon <andoune0719@gmail.com>
2020-11-21 15:35:49 +0900

    get wire-frame

--- src/pages/character/CharacterDetail.tsx
+++ src/pages/character/CharacterDetail.tsx
@@ -1,9 +1,84 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { FlexWrapper, ItemBadgeBox, RoundImage } from 'src/components';
+import { CharacterImages, ElementImages } from 'src/resources/images';
+import { characterInfo } from 'src/resources/data';
+import {} from 'src/resources/languages';
 
 interface Props {
   character: string;
 }
 
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '1000px',
+  margin: '50px auto'
+});
+
+const CharacterIntro = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  margin: '0 auto'
+});
+
+const Name = styled.div({
+  fontSize: '22px',
+  fontWeight: 'bold',
+  textAlign: 'left'
+});
+
+const Description = styled.div({
+  margin: '5px 0 0',
+  fontSize: '16px'
+});
+
 export function CharacterDetail(props: Props) {
-  return <div>{props.character}</div>;
+  return (
+    <Container>
+      <CharacterIntro>
+        <ItemBadgeBox
+          badge={
+            <RoundImage
+              src={ElementImages[characterInfo[props.character].element]}
+              styles={{
+                width: '30px',
+                height: '30px',
+                small: {
+                  width: '25px',
+                  height: '25px'
+                }
+              }}
+            />
+          }
+          child={<RoundImage src={CharacterImages[props.character]} />}
+          styles={{
+            boxStyles: { margin: '10px' },
+            tooltipStyles: { bottom: '0' }
+          }}
+          isActive={false}
+          isHoverdToolTip={false}
+          isToolTipVisible={false}
+          isRankVisible={false}
+          isBadgeVisible={true}
+        />
+        <FlexWrapper styles={{ flexDirection: 'column', margin: '0 0 0 20px', alignItems: 'flex-start' }}>
+          <Name>{props.character}</Name>
+          <Description>{characterInfo[props.character].position.join(', ')}</Description>
+          {/* Region, Weapon */}
+        </FlexWrapper>
+      </CharacterIntro>
+      {/* Nav Button */}
+      {/* Stat */}
+      {/* Skill */}
+      {/* Constellations */}
+      {/* Ascension Items */}
+      {/* Talent Level up items */}
+    </Container>
+  );
 }
