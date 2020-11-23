9ffd4434054b605782fa0972a8679c25648450f6
An Do Eon <andoune0719@gmail.com>
2020-11-23 21:41:42 +0900

    create character stat viewer

--- src/pages/character/details/CharacterStat.tsx
+++ src/pages/character/details/CharacterStat.tsx
@@ -1,3 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export function CharacterStat() {}
+import { Layout } from './Layout';
+import { FlexWrapper } from 'src/components';
+import { characterInfo, Level } from 'src/resources/data';
+
+const Container = styled.div({
+  width: '100%'
+});
+
+const LevelBox = styled.div({
+  width: '80px',
+  textAlign: 'center'
+});
+
+const StatBox = styled.div({
+  width: '80px',
+  textAlign: 'center'
+});
+
+const AdditionBox = styled.div({
+  width: '200px',
+  textAlign: 'center'
+});
+
+interface Props {
+  character: string;
+}
+
+export function CharacterStat(props: Props) {
+  const characterAscensionLevel = Object.keys(characterInfo[props.character].stats);
+
+  return (
+    <Layout title="Character Stat">
+      <Container>
+        <FlexWrapper styles={{ margin: '5px 0 0' }}>
+          <LevelBox>Max Level</LevelBox>
+          <LevelBox>Level</LevelBox>
+          <StatBox>HP</StatBox>
+          <StatBox>DEF</StatBox>
+          <StatBox>ATK</StatBox>
+          <AdditionBox>Additional</AdditionBox>
+        </FlexWrapper>
+        {characterAscensionLevel.map((level) => {
+          return (
+            <FlexWrapper styles={{ margin: '5px 0 0' }}>
+              <LevelBox>{level}</LevelBox>
+              <LevelBox>{characterInfo[props.character].stats[level as Level].Level}</LevelBox>
+              <FlexWrapper>
+                <StatBox>{characterInfo[props.character].stats[level as Level].HP}</StatBox>
+                <StatBox>{characterInfo[props.character].stats[level as Level].DEF}</StatBox>
+                <StatBox>{characterInfo[props.character].stats[level as Level].ATK}</StatBox>
+                <AdditionBox>{characterInfo[props.character].stats[level as Level].Additional}</AdditionBox>
+              </FlexWrapper>
+            </FlexWrapper>
+          );
+        })}
+      </Container>
+    </Layout>
+  );
+}
