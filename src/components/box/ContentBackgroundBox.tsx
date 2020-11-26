c4007b28276fd5b5208ee3117de046f0348d2a17
An Do Eon <andoune0719@gmail.com>
2020-11-26 22:46:13 +0900

    resize padding

--- src/components/box/ContentBackgroundBox.tsx
+++ src/components/box/ContentBackgroundBox.tsx
@@ -7,11 +7,12 @@ interface Style {
 
 const ColoredBox = styled.div<Style>((props) => {
   return {
-    padding: '20px 30px',
+    padding: '40px 30px',
     width: 'fit-content',
     margin: '0 auto',
     borderRadius: '30px',
     backgroundColor: props.color || '#313233',
+    boxShadow: '2px 2px 1px rgba(20, 20, 20, .4)',
     '@media screen and (max-width: 1380px)': {
       padding: '20px 15px'
     },
