4af224391c70ed6c1267849c730daae05586679b
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-11-28 16:28:25 +0900

    Fix PageHelmet title error

--- src/pages/character/CharacterScreen.tsx
+++ src/pages/character/CharacterScreen.tsx
@@ -6,16 +6,17 @@ import { PageHelmet } from 'src/components';
 import { CharacterList } from './CharacterList';
 import { characterInfo } from 'src/resources/data';
 import { CharacterDetailConatiner } from './CharacterDetailConatiner';
+import { Lang, trans } from 'src/resources/languages';
 
 export function CharacterScreen() {
   const characters = Object.keys(characterInfo);
 
   return (
     <ContentWrapper>
-      <ContentBackgroundBox backgroundColor="#2a2b2c">
-        <PageHelmet title={'Character'} description={'Desc'} />
+      <ContentBackgroundBox backgroundColor='#2a2b2c'>
+        <PageHelmet title={trans(Lang.Character)} description={'Desc'} />
         <Switch>
-          <Route exact path="/character" component={CharacterList} />
+          <Route exact path='/character' component={CharacterList} />
           {characters.map((name) => {
             const path = name.replace(/\s\(|\)|/g, '');
             return <Route key={name} path={`/character/${path}`} render={() => <CharacterDetailConatiner character={name} />} />;
