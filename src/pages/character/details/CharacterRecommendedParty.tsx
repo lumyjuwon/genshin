70fed523b2f35880b248787d00c7ef0a364c8d3c
An Do Eon <andoune0719@gmail.com>
2020-11-28 14:33:58 +0900

    add ContentBackgroundBox

--- src/pages/character/details/CharacterRecommendedParty.tsx
+++ src/pages/character/details/CharacterRecommendedParty.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import { Layout } from './Layout';
 import { CharacterImages, ElementImages } from 'src/resources/images';
-import { FlexWrapper, ItemBadgeBox, RoundImage, GridWrapper } from 'src/components';
+import { FlexWrapper, ItemBadgeBox, RoundImage, GridWrapper, ContentBackgroundBox, BoxModelWrapper } from 'src/components';
 import { characterInfo } from 'src/resources/data';
 import { trans, Lang } from 'src/resources/languages';
 import { Link } from 'react-router-dom';
@@ -12,6 +12,7 @@ const PartyName = styled.div({
   width: '100%',
   fontSize: '20px',
   textAlign: 'left',
+  marginBottom: '15px',
   '@media screen and (max-width: 768px)': {
     fontSize: '16px'
   }
@@ -58,71 +59,73 @@ export function CharactrerRecommendedParty(props: Props) {
                 <PartyName>
                   {trans(Lang.Recommended_Party)} #{index + INDEX_BEAUTIFY}
                 </PartyName>
-                <GridWrapper styles={{ width: '100%', medium: { width: '100%' }, small: { width: '100%' } }}>
-                  {recommendedParties[partyName].party.map((character) => {
-                    const path = character.replace(/\s\(|\)|/g, '');
-                    return (
-                      <Link key={character} to={`/character/${path}`}>
-                        <ItemBadgeBox
-                          badge={
-                            <RoundImage
-                              src={ElementImages[characterInfo[character].element]}
-                              styles={{
-                                width: '25px',
-                                height: '25px',
-                                small: {
+                <ContentBackgroundBox>
+                  <GridWrapper styles={{ width: '100%', medium: { width: '100%' }, small: { width: '100%' } }}>
+                    {recommendedParties[partyName].party.map((character) => {
+                      const path = character.replace(/\s\(|\)|/g, '');
+                      return (
+                        <Link key={character} to={`/character/${path}`}>
+                          <ItemBadgeBox
+                            badge={
+                              <RoundImage
+                                src={ElementImages[characterInfo[character].element]}
+                                styles={{
                                   width: '25px',
-                                  height: '25px'
-                                }
-                              }}
-                            />
-                          }
-                          child={<RoundImage src={CharacterImages[character]} styles={{ width: '80px', height: '80px' }} />}
-                          onClick={() => goToScrollTop()}
-                          styles={{
-                            boxStyles: { margin: '10px' },
-                            tooltipStyles: { bottom: '0', fontSize: '13px' }
-                          }}
-                          hoverInnerColor={'#f1f2f3'}
-                          tooltip={character}
-                          isActive={true}
-                          isHoverdToolTip={true}
-                          isToolTipVisible={true}
-                          isRankVisible={false}
-                          isBadgeVisible={true}
-                        />
-                      </Link>
-                    );
-                  })}
-                </GridWrapper>
-                <>
-                  {alterCharacter && (
-                    <>
-                      <AlterText>{trans(Lang.Alternative)}</AlterText>
-                      <FlexWrapper styles={{ flexDirection: 'column' }}>
-                        {Object.keys(alterCharacter).map((character) => {
-                          return (
-                            <FlexWrapper key={character} styles={{ margin: '0 0 10px' }}>
-                              <>
-                                <RoundImage src={CharacterImages[character]} styles={{ width: '50px', height: '50px' }} />
-                                &nbsp;→&nbsp;
-                                {alterCharacter[character].map((altCharacter) => {
-                                  return (
-                                    <RoundImage
-                                      key={altCharacter}
-                                      src={CharacterImages[altCharacter]}
-                                      styles={{ width: '50px', height: '50px' }}
-                                    />
-                                  );
-                                })}
-                              </>
-                            </FlexWrapper>
-                          );
-                        })}
-                      </FlexWrapper>
-                    </>
-                  )}
-                </>
+                                  height: '25px',
+                                  small: {
+                                    width: '25px',
+                                    height: '25px'
+                                  }
+                                }}
+                              />
+                            }
+                            child={<RoundImage src={CharacterImages[character]} styles={{ width: '80px', height: '80px' }} />}
+                            onClick={() => goToScrollTop()}
+                            styles={{
+                              boxStyles: { margin: '10px' },
+                              tooltipStyles: { bottom: '0', fontSize: '13px' }
+                            }}
+                            hoverInnerColor={'#f1f2f3'}
+                            tooltip={character}
+                            isActive={true}
+                            isHoverdToolTip={true}
+                            isToolTipVisible={true}
+                            isRankVisible={false}
+                            isBadgeVisible={true}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </GridWrapper>
+                  <>
+                    {alterCharacter && (
+                      <>
+                        <AlterText>{trans(Lang.Alternative)}</AlterText>
+                        <FlexWrapper styles={{ flexDirection: 'column' }}>
+                          {Object.keys(alterCharacter).map((character) => {
+                            return (
+                              <FlexWrapper key={character} styles={{ margin: '0 0 10px' }}>
+                                <>
+                                  <RoundImage src={CharacterImages[character]} styles={{ width: '50px', height: '50px' }} />
+                                  &nbsp;→&nbsp;
+                                  {alterCharacter[character].map((altCharacter) => {
+                                    return (
+                                      <RoundImage
+                                        key={altCharacter}
+                                        src={CharacterImages[altCharacter]}
+                                        styles={{ width: '50px', height: '50px' }}
+                                      />
+                                    );
+                                  })}
+                                </>
+                              </FlexWrapper>
+                            );
+                          })}
+                        </FlexWrapper>
+                      </>
+                    )}
+                  </>
+                </ContentBackgroundBox>
               </FlexWrapper>
             );
           })}
