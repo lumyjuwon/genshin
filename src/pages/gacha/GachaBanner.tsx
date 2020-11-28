b6ba3ba9797c243b2935fc8717d35a4cef74e44a
An Do Eon <andoune0719@gmail.com>
2020-11-28 21:49:45 +0900

    use CSSGridWrapper

--- src/pages/gacha/GachaBanner.tsx
+++ src/pages/gacha/GachaBanner.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-import { BoxModelWrapper, DropDownButton, FlexWrapper, RoundImageButton } from 'src/components';
+import { BoxModelWrapper, CSSGridWrapper, DropDownButton, FlexWrapper, GridWrapper, RoundImageButton } from 'src/components';
 import { trans, Lang, KeyLang } from 'src/resources/languages';
 import { WishClickImages } from 'src/resources/images';
 import { GachaDetails } from './GachaDetails';
@@ -30,39 +30,56 @@ export function GachaBanner(props: Props) {
   }
 
   return (
-    <FlexWrapper styles={{ justifyContent: 'space-between', margin: '0 0 20px' }}>
+    <FlexWrapper styles={{ justifyContent: 'space-between', margin: '0 0 20px', alignItems: 'flex-end' }}>
       <>
         {props.video ? (
-          <FlexWrapper styles={{ alignItems: 'flex-end' }}>
+          <CSSGridWrapper
+            styles={{
+              gridTemplateColumns: 'repeat(auto-fit, 130px)',
+              placeContent: 'left center',
+              columnGap: '20px',
+              margin: '0',
+              medium: { gridTemplateColumns: 'repeat(auto-fit, 130px)', columnGap: '20px' },
+              small: { gridTemplateColumns: 'repeat(auto-fit, 100px)', columnGap: '12px', rowGap: '12px' }
+            }}
+          >
             {props.pickUpList.map((content, index) => {
               return (
-                <BoxModelWrapper styles={{ margin: '0 20px 0 0', medium: { margin: '0 20px 0 0' } }}>
-                  <RoundImageButton
-                    src={getImagePath(content, index)}
-                    onClick={() => onClickBannerImages(index)}
-                    styles={{
-                      imageStyles: { width: '130px', height: '65px' },
-                      buttonStyles: { pointerEvents: 'none', border: 'none' }
-                    }}
-                  />
-                </BoxModelWrapper>
+                <RoundImageButton
+                  src={getImagePath(content, index)}
+                  onClick={() => onClickBannerImages(index)}
+                  styles={{
+                    imageStyles: { width: '130px', height: '65px', small: { width: '80px', height: '40px' } },
+                    buttonStyles: { border: 'none', margin: '0', pointerEvents: 'none' }
+                  }}
+                />
               );
             })}
-          </FlexWrapper>
+          </CSSGridWrapper>
         ) : (
-          <FlexWrapper styles={{ alignItems: 'flex-end' }}>
+          <CSSGridWrapper
+            styles={{
+              gridTemplateColumns: 'repeat(auto-fit, 130px)',
+              placeContent: 'left center',
+              columnGap: '20px',
+              margin: '0',
+              medium: { gridTemplateColumns: 'repeat(auto-fit, 130px)', columnGap: '20px' },
+              small: { gridTemplateColumns: 'repeat(auto-fit, 100px)', columnGap: '12px', rowGap: '12px' }
+            }}
+          >
             {props.pickUpList.map((content, index) => {
               return (
-                <BoxModelWrapper styles={{ margin: '0 20px 0 0', medium: { margin: '0 20px 0 0' } }}>
-                  <RoundImageButton
-                    src={getImagePath(content, index)}
-                    onClick={() => onClickBannerImages(index)}
-                    styles={{ imageStyles: { width: '130px', height: '65px' }, buttonStyles: { border: 'none' } }}
-                  />
-                </BoxModelWrapper>
+                <RoundImageButton
+                  src={getImagePath(content, index)}
+                  onClick={() => onClickBannerImages(index)}
+                  styles={{
+                    imageStyles: { width: '130px', height: '65px', small: { width: '80px', height: '40px' } },
+                    buttonStyles: { border: 'none', margin: '0' }
+                  }}
+                />
               );
             })}
-          </FlexWrapper>
+          </CSSGridWrapper>
         )}
         <GachaDetails content={props.content} />
       </>
